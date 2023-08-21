e1c32963276360d7fed43a12997c5efd303b2779
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2023-08-21 12:29:00 -0700

    added carousel

---
+++ client/src/components/single-listing-components/SingleListingCarousel.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Carousel from 'react-material-ui-carousel'
+import { Paper } from '@mui/material'
+
+
+const SingleListingCarousel = ({ images }) => {
+  console.log(images);
+
+  var items = [
+    {
+      name: "Random Name #1",
+      description: "Probably the most random thing you have ever seen!",
+      imgUrl: "https://images.unsplash.com/photo-1576941089067-2de3c901e126?ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGhvdXNlfGVufDB8fDB8fHww&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+    },
+    {
+      name: "Random Name #2",
+      description: "Hello World!",
+      imgUrl: "https://images.unsplash.com/photo-1598228723793-52759bba239c?ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGhvdXNlfGVufDB8fDB8fHww&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+    },
+    {
+      name: "Random Name #3",
+      description: "Hello World!",
+      imgUrl: "https://media.istockphoto.com/id/1357529194/photo/3d-rendering-of-a-modern-styled-living-room-with-fireplace.jpg?s=612x612&amp;w=0&amp;k=20&amp;c=bBBhHQnZLal9xoH9fGhAEg33CDVF0msBfB7l9cLTU_s="
+    }
+  ]
+
+  return (
+    <Carousel>
+      {
+        items.map((item, i) =>
+          <Paper key={i}>
+            <h2>{item.name}</h2>
+            <p>{item.description}</p>
+            <img src={item.imgUrl} alt={item.name} />
+
+
+          </Paper>)
+      }
+    </Carousel>
+  )
+}
+
+
+export default SingleListingCarousel
--- client/src/views/SingleListing.jsx
+++ client/src/views/SingleListing.jsx
@@ -2,23 +2,24 @@ import React from 'react'
 import { useParams } from "react-router";
 import { useEffect, useState } from 'react'
 import axios from "axios";
+import SingleListingCarousel from '../components/single-listing-components/SingleListingCarousel'
 
 const SingleListing = () => {
   const { id } = useParams()
   const [listing, setListing] = useState({
-    name: '',
-    type: '',
-    numOfBedrooms: '',
-    numOfBathrooms: '',
-    size: '',
-    description: '',
-    price: '',
-    isFeatured: false,
-    address: '',
-    city: '',
-    state: '',
-    zipCode: '',
-    imgUrl: '',
+    // name: '',
+    // type: '',
+    // numOfBedrooms: '',
+    // numOfBathrooms: '',
+    // size: '',
+    // description: '',
+    // price: '',
+    // isFeatured: false,
+    // address: '',
+    // city: '',
+    // state: '',
+    // zipCode: '',
+    // imgUrl: '',
   })
 
   useEffect(() => {
@@ -38,6 +39,8 @@ const SingleListing = () => {
     <div>
       <h1>Display something</h1>
       <h3>{listing.name}</h3>
+      <SingleListingCarousel images={listing.imgUrl} />
+
 
     </div>
   )
